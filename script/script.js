e9bed1a8a43113926fce6cf00449b483ad892b47
Gustavo <gustavo.murillo107@gmail.com>
2021-12-07 15:22:45 -0300

    Animation, add sections, NavBar changed

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('btnativar');
+const navbar = document.getElementById('navbar');
+
+button.addEventListener('click', () => {
+    if(navbar.style.display == "none"){
+        navbar.style.display = "block";
+    } else if(navbar.style.display = "block"){
+        navbar.style.display = "none";
+    }
+    
+})
